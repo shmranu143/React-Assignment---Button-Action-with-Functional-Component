39ac7e5ae9f7ab8cc7ee33b80e3f232113edf6b1
shmranu143 <recs.cse1641@gmail>
2021-02-17 15:27:14 +0530

    last

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,16 @@ function App() {
   const handleClick=()=>{
     setClick({isClicked:true})
   }
+  const addPara=()=>{
+    if(isClicked){
+      return <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    }
+  }
   return (
     <div id="main">
       {/* // Do not alter the main div */}
       <button id="click" onClick={handleClick}>click me</button>
-      {isClicked && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      {addPara()}
     </div>
   );
 }
